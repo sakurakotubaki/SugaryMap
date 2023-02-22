af7cea0982a68efb63d91db8c201ea578a720d2b
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2023-02-23 00:50:51 +0900

    delete Themeを削除

--- lib/theme/button_theme.dart
+++
@@ -1,8 +0,0 @@
-import 'package:sugary_map/service/export/global_export.dart';
-
-enum MyButton {
-  appButton(appColor: Colors.black87);
-
-  const MyButton({required this.appColor});
-  final Color appColor;
-}
