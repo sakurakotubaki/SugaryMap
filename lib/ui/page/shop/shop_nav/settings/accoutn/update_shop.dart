57bc39bb5b42c1246b8f5e5536716a99d18f3de6
Kenta Wakasa <wakasa.kenta.pg@gmail.com>
2023-02-23 14:16:59 +0900

    add const

--- lib/ui/page/shop/shop_nav/settings/accoutn/update_shop.dart
+++ lib/ui/page/shop/shop_nav/settings/accoutn/update_shop.dart
@@ -1,8 +1,7 @@
 import 'package:sugary_map/service/export/global_export.dart';
-import 'package:sugary_map/ui/page/shop/shop_nav/admin_page/admin_page.dart';
 
 class UpdateShop extends StatefulWidget {
-  UpdateShop({Key? key}) : super(key: key);
+  const UpdateShop({Key? key}) : super(key: key);
 
   static const routeName = 'UpdateShop';
 
@@ -20,19 +19,19 @@ class _UpdateShopState extends State<UpdateShop> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            SizedBox(height: 50),
+            const SizedBox(height: 50),
             Stack(
               children: [
                 Container(
                   clipBehavior: Clip.antiAlias,
                   width: 160,
                   height: 160,
-                  decoration: const BoxDecoration(
+                  decoration: BoxDecoration(
                     shape: BoxShape.circle,
-                    color: Colors.blueGrey,
+                    color: Colors.grey[200],
                   ),
                 ),
-                Positioned(
+                const Positioned(
                   right: 10,
                   bottom: 10,
                   child: CircleAvatar(
@@ -46,138 +45,138 @@ class _UpdateShopState extends State<UpdateShop> {
                 )
               ],
             ),
-            SizedBox(height: 20),
-            Container(width: 300, child: Text('Twitterアカウントを登録')),
-            SizedBox(height: 10),
-            Container(
+            const SizedBox(height: 20),
+            const SizedBox(width: 300, child: Text('Twitterアカウントを登録')),
+            const SizedBox(height: 10),
+            SizedBox(
               width: 300,
               child: TextFormField(
                 decoration: InputDecoration(
-                    contentPadding: EdgeInsets.only(left: 20),
+                    contentPadding: const EdgeInsets.only(left: 20),
                     enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Colors.grey)),
+                        borderSide: const BorderSide(color: Colors.grey)),
                     labelText: "https://twitter.com/の後から入力"),
               ),
             ),
-            SizedBox(height: 20),
-            Container(width: 300, child: Text('Instagramアカウントを登録')),
-            SizedBox(height: 10),
-            Container(
+            const SizedBox(height: 20),
+            const SizedBox(width: 300, child: Text('Instagramアカウントを登録')),
+            const SizedBox(height: 10),
+            SizedBox(
               width: 300,
               child: TextFormField(
                 decoration: InputDecoration(
-                    contentPadding: EdgeInsets.only(left: 20),
+                    contentPadding: const EdgeInsets.only(left: 20),
                     enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Colors.grey)),
+                        borderSide: const BorderSide(color: Colors.grey)),
                     labelText: "https://www.instagram.com/の後から入力"),
               ),
             ),
-            SizedBox(height: 20),
-            Container(width: 300, child: Text('Facebookアカウントを登録')),
-            SizedBox(height: 10),
-            Container(
+            const SizedBox(height: 20),
+            const SizedBox(width: 300, child: Text('Facebookアカウントを登録')),
+            const SizedBox(height: 10),
+            SizedBox(
               width: 300,
               child: TextFormField(
                 decoration: InputDecoration(
-                    contentPadding: EdgeInsets.only(left: 20),
+                    contentPadding: const EdgeInsets.only(left: 20),
                     enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Colors.grey)),
+                        borderSide: const BorderSide(color: Colors.grey)),
                     labelText: "https://www.facebook.com/の後から入力"),
               ),
             ),
-            SizedBox(height: 20),
-            Container(width: 300, child: Text('ホームページを登録')),
-            SizedBox(height: 10),
-            Container(
+            const SizedBox(height: 20),
+            const SizedBox(width: 300, child: Text('ホームページを登録')),
+            const SizedBox(height: 10),
+            SizedBox(
               width: 300,
               child: TextFormField(
                 decoration: InputDecoration(
-                    contentPadding: EdgeInsets.only(left: 20),
+                    contentPadding: const EdgeInsets.only(left: 20),
                     enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Colors.grey)),
+                        borderSide: const BorderSide(color: Colors.grey)),
                     labelText: "ホームページのURLを全て貼り付ける"),
               ),
             ),
-            SizedBox(height: 10),
+            const SizedBox(height: 10),
 
             ///[カレンダーのようなものを配置]
 
-            SizedBox(height: 20),
-            Text('住所'),
-            SizedBox(height: 10),
-            Container(
+            const SizedBox(height: 20),
+            const Text('住所'),
+            const SizedBox(height: 10),
+            SizedBox(
               width: 300,
               child: TextFormField(
                 decoration: InputDecoration(
-                    contentPadding: EdgeInsets.only(left: 20),
+                    contentPadding: const EdgeInsets.only(left: 20),
                     enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Colors.grey)),
+                        borderSide: const BorderSide(color: Colors.grey)),
                     labelText: "住所を入力"),
               ),
             ),
-            SizedBox(height: 20),
-            Container(width: 300, child: Text('電話番号')),
-            SizedBox(height: 10),
-            Container(
+            const SizedBox(height: 20),
+            const SizedBox(width: 300, child: Text('電話番号')),
+            const SizedBox(height: 10),
+            SizedBox(
               width: 300,
               child: TextFormField(
                 decoration: InputDecoration(
-                    contentPadding: EdgeInsets.only(left: 20),
+                    contentPadding: const EdgeInsets.only(left: 20),
                     enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Colors.grey)),
+                        borderSide: const BorderSide(color: Colors.grey)),
                     labelText: "電話番号をハイフンなしで入力"),
               ),
             ),
-            SizedBox(height: 20),
-            Container(width: 300, child: Text('交通手段')),
-            SizedBox(height: 10),
-            Container(
+            const SizedBox(height: 20),
+            const SizedBox(width: 300, child: Text('交通手段')),
+            const SizedBox(height: 10),
+            SizedBox(
               width: 300,
               child: TextFormField(
                 decoration: InputDecoration(
-                    contentPadding: EdgeInsets.only(left: 20),
+                    contentPadding: const EdgeInsets.only(left: 20),
                     enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Colors.grey)),
+                        borderSide: const BorderSide(color: Colors.grey)),
                     labelText: "交通手段を入力"),
               ),
             ),
-            SizedBox(height: 20),
-            Container(width: 300, child: Text('駐車場')),
-            SizedBox(height: 10),
-            Container(
+            const SizedBox(height: 20),
+            const SizedBox(width: 300, child: Text('駐車場')),
+            const SizedBox(height: 10),
+            SizedBox(
               width: 300,
               child: TextFormField(
                 decoration: InputDecoration(
-                    contentPadding: EdgeInsets.only(left: 20),
+                    contentPadding: const EdgeInsets.only(left: 20),
                     enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Colors.grey)),
+                        borderSide: const BorderSide(color: Colors.grey)),
                     labelText: "駐車場の有無を入力"),
               ),
             ),
-            SizedBox(height: 20),
-            Container(width: 300, child: Text('お店の紹介を登録')),
+            const SizedBox(height: 20),
+            const SizedBox(width: 300, child: Text('お店の紹介を登録')),
             Container(),
-            Container(
+            SizedBox(
               width: 300,
               child: TextFormField(
                 maxLines: 5,
                 keyboardType: TextInputType.multiline,
-                decoration: InputDecoration(
+                decoration: const InputDecoration(
                     hintText: "例: マカロン２０種類を販売する専門店",
                     border: OutlineInputBorder()),
               ),
             ),
-            SizedBox(height: 20),
-            Container(width: 300, child: Text('画像は４枚まで投稿できます')),
-            SizedBox(height: 10),
+            const SizedBox(height: 20),
+            const SizedBox(width: 300, child: Text('画像は４枚まで投稿できます')),
+            const SizedBox(height: 10),
             Stack(
               children: [
                 Container(
@@ -185,7 +184,7 @@ class _UpdateShopState extends State<UpdateShop> {
                   height: 150,
                   decoration: BoxDecoration(border: Border.all(width: 1)),
                 ),
-                Positioned(
+                const Positioned(
                   bottom: 55,
                   right: 55,
                   child: Icon(
@@ -196,7 +195,7 @@ class _UpdateShopState extends State<UpdateShop> {
                 )
               ],
             ),
-            SizedBox(height: 40),
+            const SizedBox(height: 40),
             SizedBox(
               width: 200,
               height: 50,
@@ -206,11 +205,11 @@ class _UpdateShopState extends State<UpdateShop> {
                   onPressed: () {
                     context.go('/admin');
                   },
-                  child: Text(
+                  child: const Text(
                     '保存',
                   )),
             ),
-            SizedBox(height: 50),
+            const SizedBox(height: 50),
           ],
         ),
       ),
