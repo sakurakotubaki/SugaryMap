2524cb994acd53d67a44bfbafb23f5b52ee550b5
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2023-03-29 22:47:30 +0900

    add ログイン状態によってWidgetを切り替える

--- lib/ui/page/user/user_nav/mypage/mypage.dart
+++ lib/ui/page/user/user_nav/mypage/mypage.dart
@@ -1,8 +1,9 @@
-// ignore_for_file: unused_import, unnecessary_import
+// ignore_for_file: unused_import, unnecessary_import, prefer_const_constructors, avoid_unnecessary_containers
 
 import 'package:sugary_map/constant/privacy_const.dart';
 import 'package:sugary_map/service/export/global_export.dart';
 import 'package:sugary_map/service/export/user_nabbar_export.dart';
+import 'package:sugary_map/service/router/auth_provider.dart';
 import 'package:sugary_map/ui/auth_page/pages/signin_page.dart';
 import 'package:sugary_map/ui/component/global/custom_divider.dart';
 import 'package:sugary_map/ui/page/user/user_nav/mypage/bookmark_page.dart';
@@ -13,28 +14,25 @@ import 'package:sugary_map/ui/page/user/user_nav/mypage/user_notification.dart';
 import 'package:sugary_map/ui/page/user/user_nav/mypage/user_terms.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class MyPage extends StatefulWidget {
-  const MyPage({Key? key}) : super(key: key);
+class MyPage extends ConsumerWidget {
+  const MyPage({super.key});
 
   static const routeName = 'myPage';
 
   @override
-  State<MyPage> createState() => _MyPageState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    // ユーザーのログイン状態を監視する
+    final authStateAsync = ref.watch(authProvider);
 
-class _MyPageState extends State<MyPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+    return authStateAsync.when(
+        loading: () => const CircularProgressIndicator(),
+        error: (err, stack) => Text('Error: $err'),
+        data: (user) {
+          // nullじゃなかったら、ログインしているWidgetを表示
+          return user != null ? Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        actions: [
-          IconButton(
-              onPressed: () {
-                GoRouter.of(context).goNamed(UserNotification.routeName);
-              },
-              icon: const Icon(Icons.notifications))
-        ],
-        title: Text('マイページ'),
+        title: const Text('マイページ'),
       ),
       body: Center(
         child: Column(
@@ -64,6 +62,7 @@ class _MyPageState extends State<MyPage> {
                       const Text('こんぶさん'),
                       SizedBox(height: 20),
                       Row(
+                        // ignore: prefer_const_literals_to_create_immutables
                         children: [
                           Text('称号'),
                           SizedBox(width: 20),
@@ -164,6 +163,113 @@ class _MyPageState extends State<MyPage> {
           ],
         ),
       ),
+      // nullだったらログインしていないWidgetを表示する
+    ) : Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('マイページ'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 20, left: 20, bottom: 20),
+              child: Row(
+                children: [
+                  Container(
+                    child: Icon(Icons.person),
+                  ),
+                  const SizedBox(width: 20),
+                  Column(
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      const Text('アカウントが登録されていません'),
+                      SizedBox(height: 20),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      GoRouter.of(context).goNamed(BookmarkPage.routeName);
+                    },
+                    child: ListTile(
+                      trailing: Icon(Icons.arrow_forward_ios),
+                      title: Text('お気に入り'),
+                    ),
+                  ),
+                  CustomDivider(),
+                  GestureDetector(
+                    onTap: () {
+                      GoRouter.of(context).goNamed(ManualPage.routeName);
+                    },
+                    child: ListTile(
+                      trailing: Icon(Icons.arrow_forward_ios),
+                      title: Text('使い方'),
+                    ),
+                  ),
+                  CustomDivider(),
+                  GestureDetector(
+                    onTap: () {
+                      GoRouter.of(context)
+                          .goNamed(UserAccountSettings.routeName);
+                    },
+                    child: ListTile(
+                      trailing: Icon(Icons.arrow_forward_ios),
+                      title: Text('アカウント設定'),
+                    ),
+                  ),
+                  CustomDivider(),
+                  GestureDetector(
+                    onTap: () {
+                      launchUrl(Uri.parse(privacyUrl));
+                    },
+                    child: ListTile(
+                      trailing: Icon(Icons.arrow_forward_ios),
+                      title: Text('プライバシーポリシー'),
+                    ),
+                  ),
+                  CustomDivider(),
+                  GestureDetector(
+                    onTap: () {
+                      launchUrl(Uri.parse(termsUrl));
+                    },
+                    child: ListTile(
+                      trailing: Icon(Icons.arrow_forward_ios),
+                      title: Text('利用規約'),
+                    ),
+                  ),
+                  CustomDivider(),
+                  GestureDetector(
+                    onTap: () {
+                      GoRouter.of(context).goNamed(UserInquiry.routeName);
+                    },
+                    child: ListTile(
+                      trailing: Icon(Icons.arrow_forward_ios),
+                      title: Text('お問い合わせ'),
+                    ),
+                  ),
+                  CustomDivider(),
+                  GestureDetector(
+                    onTap: () {
+                      context.goNamed(SignInPage.routeName);
+                    },
+                    child: ListTile(
+                      trailing: Icon(Icons.arrow_forward_ios),
+                      title: Text('ログイン'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
+        });
   }
 }
