1ed33d2b0435b1cba38a58cba49be29d40030c74
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2023-04-14 19:27:22 +0900

    remove 不要な旧ファイルを削除

--- lib/presentation/ui/page/auth_page/auth_service/firebase_auth_provider.dart
+++
@@ -1,6 +0,0 @@
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:sugary_map/presentation/export/global_export.dart';
-
-// final firebaseAuthProvider = Provider<FirebaseAuth>((ref) {
-//   return FirebaseAuth.instance;
-// });
