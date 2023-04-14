d1def2c950a316ab88e29051b711fdb536b646fd
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2023-04-14 16:18:29 +0900

    add 認証のロジック

---
+++ lib/presentation/auth_service/sign_in_service.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final signInServiceProvider = Provider<SignInService>((ref) => SignInService());
+
+class SignInService {
+  Future<UserCredential> signIn(String email, String password) async {
+    try {
+      final result = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return result;
+    } on FirebaseAuthException catch (e) {
+      throw _handleError(e.code);
+    } catch (e) {
+      throw 'エラーが発生しました。';
+    }
+  }
+
+  String _handleError(String errorCode) {
+    switch (errorCode) {
+      case 'invalid-email':
+        return 'メールアドレスが無効。';
+      case 'user-disabled':
+        return 'このアカウントは無効になっています。';
+      case 'user-not-found':
+        return 'アカウントが見つかりません。';
+      case 'wrong-password':
+        return 'パスワードが一致しません。';
+      default:
+        return 'エラーが発生しました。';
+    }
+  }
+}
---
+++ lib/presentation/auth_service/sign_up_service.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final signUpServiceProvider = Provider<SignUpService>((ref) => SignUpService());
+
+class SignUpService {
+  Future<UserCredential> signIn(String email, String password) async {
+    try {
+      final result = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return result;
+    } on FirebaseAuthException catch (e) {
+      throw _handleError(e.code);
+    } catch (e) {
+      throw 'エラーが発生しました。';
+    }
+  }
+
+  String _handleError(String errorCode) {
+    switch (errorCode) {
+      case 'invalid-email':
+        return 'メールアドレスが無効。';
+      case 'user-disabled':
+        return 'このアカウントは無効になっています。';
+      case 'user-not-found':
+        return 'アカウントが見つかりません。';
+      case 'wrong-password':
+        return 'パスワードが一致しません。';
+      default:
+        return 'エラーが発生しました。';
+    }
+  }
+}
